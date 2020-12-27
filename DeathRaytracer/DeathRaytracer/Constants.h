9df140bbca62af40f2583dd910c67472fc7131d9
Marcus Lilja <marcus.t.lilja@gmail.com>
2020-12-27 12:38:53 +0100

    Settings file (before we add config files...) 💪🏿

---
+++ DeathRaytracer/DeathRaytracer/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "DataTypes.h"
+
+namespace Settings
+{
+	uint32 IMAGE_SIZE_X = 256;
+	uint32 IMAGE_SIZE_Y = 256;
+}
--- DeathRaytracer/DeathRaytracer/main.cpp
+++ DeathRaytracer/DeathRaytracer/main.cpp
@@ -1,8 +1,9 @@
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
+#include "Constants.h"
+
+int main(int argc, const char* argv[])
+{
 	std::cout << "Hello, World!\n";
 	return 0;
 }
